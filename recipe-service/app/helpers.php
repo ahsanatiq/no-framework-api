6994f16b0504f2ea03ac80c0ee3fa2c53b3e87c5
Ahsan <iamahsan82@gmail.com>
2018-10-29 21:57:25 +0500

    add helper functions to enable us write clean and simple code

--- recipe-service/app/helpers.php
+++ recipe-service/app/helpers.php
@@ -1,1 +1,22 @@
-<?php 
+<?php
+
+if (!function_exists('app')) {
+    function app()
+    {
+        return \App\Application::getInstance();
+    }
+}
+
+if (!function_exists('container')) {
+    function container()
+    {
+        return app()->getContainer();
+    }
+}
+
+if (!function_exists('config')) {
+    function config()
+    {
+        return container()->make('config');
+    }
+}
