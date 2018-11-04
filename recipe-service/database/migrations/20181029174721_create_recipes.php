62968095e309a88ae3a6f915b4eb832be263cf9d
Ahsan <iamahsan82@gmail.com>
2018-11-05 01:52:41 +0500

    added deleted_at column in the table to support soft deletes

--- recipe-service/database/migrations/20181029174721_create_recipes.php
+++ recipe-service/database/migrations/20181029174721_create_recipes.php
@@ -2,6 +2,7 @@
 
 
 use Phinx\Migration\AbstractMigration;
+use Phinx\Util\Literal;
 
 class CreateRecipes extends AbstractMigration
 {
@@ -13,9 +14,10 @@ class CreateRecipes extends AbstractMigration
         ->addColumn('description', 'text')
         ->addColumn('prep_time', 'integer', ['limit' => 4])
         ->addColumn('difficulty', 'integer', ['limit' => 1])
-        ->addColumn('vegetarian', 'boolean', ['default' => false, 'null' => true])
-        ->addColumn('created_at', 'datetime', ['null' => true])
-        ->addColumn('updated_at', 'datetime', ['null' => true])
+        ->addColumn('vegetarian', 'boolean', ['null' => false, 'default' => false])
+        ->addColumn('created_at', 'datetime', ['null' => false, 'default' => Literal::from('now()')])
+        ->addColumn('updated_at', 'datetime', ['null' => false, 'default' => Literal::from('now()')])
+        ->addColumn('deleted_at', 'datetime', ['null' => true])
         ->create();
     }
 }
