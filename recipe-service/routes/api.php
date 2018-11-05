442e4731ff7b110b38a0b40de816ad719f90cd6c
Ahsan <iamahsan82@gmail.com>
2018-11-05 18:30:10 +0500

    create all routes

--- recipe-service/routes/api.php
+++ recipe-service/routes/api.php
@@ -3,11 +3,11 @@
 $router->prefix('/api/v1/')->namespace('App\Controllers')->group(function ($router) {
 
     $router->get('recipes', 'RecipeController@getList');
-    // $router->post('recipes', 'RecipeController@create', [$auth]);
+    $router->post('recipes', 'RecipeController@create');
     $router->get('recipes/{id}', 'RecipeController@get');
-    // $router->put('recipes/{id}', 'RecipeController@update', [$auth]);
-    // $router->patch('recipes/{id}', 'RecipeController@update', [$auth]);
-    // $router->delete('recipes/{id}', 'RecipeController@delete', [$auth]);
-    $router->post('recipes/{id}/rating', 'RecipeController@createRating');
+    $router->put('recipes/{id}', 'RecipeController@update');
+    $router->patch('recipes/{id}', 'RecipeController@update');
+    $router->delete('recipes/{id}', 'RecipeController@delete');
+    $router->post('recipes/{id}/rating', 'RatingController@create');
 
 });
