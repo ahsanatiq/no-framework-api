5723f5f5eddb749b2f4fced89511ef9ce4392e86
Ahsan <iamahsan82@gmail.com>
2018-11-07 06:44:50 +0500

    code cleanup

--- recipe-service/tests/acceptance/WelcomeCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform test on root endpoint and expect 404 Not Found');
-$I->sendGET('');
-$I->seeResponseCodeIs(404);
-// $I->see('Hello');
-
