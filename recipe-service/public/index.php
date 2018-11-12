fea081780667704d5e3f9849d7a7f633f8bfa5d0
Ahsan <iamahsan82@gmail.com>
2018-11-12 06:50:04 +0500

    code refactor

--- recipe-service/public/index.php
+++ recipe-service/public/index.php
@@ -1,4 +1,4 @@
 <?php
 
-$app = require(__DIR__ . '/../bootstrap/app.php');
-$app->run();
+require(__DIR__ . '/../bootstrap/app.php');
+app()->run();
--- recipe-service/tests/acceptance/_bootstrap.php
+++ recipe-service/tests/acceptance/_bootstrap.php
@@ -2,3 +2,4 @@
 
 loadEnvironmentFromFile(codecept_root_dir('.env.testing'));
 require codecept_root_dir('bootstrap/app.php');
+app();
