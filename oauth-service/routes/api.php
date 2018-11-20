3310355d25bc3aded9f27a189c058f80acf28df8
Ahsan <iamahsan82@gmail.com>
2018-11-20 06:01:44 +0500

    api version the oauth routes

--- oauth-service/routes/api.php
+++ oauth-service/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-$router->prefix('/oauth/')->namespace('App\Controllers')->group(
+$router->prefix('api/v1/oauth/')->namespace('App\Controllers')->group(
     function ($router) {
         $router->post('token', 'OauthController@getToken');
         $router->post('authorize', 'OauthController@getAuthorize');
